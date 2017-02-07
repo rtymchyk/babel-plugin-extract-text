b321ca5d788f37fd2389b26bb70d594d0f7ff04a
Roman Tymchyk <roman@tilt.com>
2017-02-07 14:49:22 -0500

    Update package.json

--- anotherSampleCode.js
+++
@@ -1,9 +0,0 @@
-class Demo2 extends React.Component {
-  render() {
-    return (
-      <span>
-        <LocalizedString id="Hello World!" idPlural="Hello World 2!"/>
-      </span>
-    );
-  }
-}
