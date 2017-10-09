258fb8fe4a4e7dbb3a9536bfc8ad0713cbb8dcce
Roman Tymchyk <rtymchyk@gmail.com>
2017-10-09 15:21:30 -0400

    Don't cache entries between plugin executions

--- plugin.js
+++ plugin.js
@@ -13,24 +13,25 @@ const {
 } = require('./src/builders')
 
 module.exports = ({ types }) => {
-  const entries = []
-
   return {
+    pre (state) {
+      this.entries = []
+    },
     visitor: {
       CallExpression (path, state) {
         const entry = buildCallExpressionEntry(types, path, state)
-        if (entry) entries.push(entry)
+        if (entry) this.entries.push(entry)
       },
       JSXElement (path, state) {
         const entry = buildJSXElementEntry(types, path, state)
-        if (entry) entries.push(entry)
+        if (entry) this.entries.push(entry)
       },
     },
     post (state) {
       const thisPlugin = state.opts.plugins.filter(plugin => plugin[0].key === PLUGIN_KEY)[0]
       const args = thisPlugin[1] || {}
       const outputFile = args.outputFile || DEFAULT_OUTPUT_FILE
-      const poRawData = mergeEntries(args, entries)
+      const poRawData = mergeEntries(args, this.entries)
       const po = gettextParser.po.compile(poRawData)
 
       fs.writeFileSync(outputFile, po)
