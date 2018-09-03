8a107d50f22b44fd2047445ee9ae2fe8c485a4c1
Roman Tymchyk <rtymchyk@gmail.com>
2018-09-02 22:59:40 -0400

    Babel 7 Compatibility

--- plugin.js
+++ plugin.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const path = require('path')
 
 const PLUGIN_KEY = process.env.NODE_ENV === 'test'
-  ? './plugin.js' : 'extract-text'
+  ? 'plugin.js' : 'extract-text'
 const DEFAULT_OUTPUT_FILE = 'strings.po'
 
 const {
@@ -31,14 +31,15 @@ module.exports = ({ types }) => {
     post (state) {
       if (this.entries.length === 0) return
 
-      const thisPlugin = state.opts.plugins.filter(plugin => plugin[0].key === PLUGIN_KEY)[0]
-      const args = thisPlugin[1] || {}
+      const thisPlugin = state.opts.plugins.find(plugin => plugin.key.includes(PLUGIN_KEY))
+      const args = (thisPlugin || {}).options
 
-      const currentFileName = path.basename(this.file.opts.filename)
-      const currentFileDir = path.dirname(this.file.opts.filename)
+      const filename = this.file.opts.filename || 'unknown'
+      const currentFileName = path.basename(filename)
+      const currentFileDir = path.dirname(filename)
 
       const outputFile = args.outputFile ||
-        (currentFileName !== 'unknown' && `${currentFileName}.po`) ||
+        (filename !== 'unknown' && `${currentFileName}.po`) ||
         DEFAULT_OUTPUT_FILE
       const outputDir = args.outputDir || currentFileDir
 
